20f8ca3119bfcd4f28bd79d149fed966c9e92976
Samuel-de-Oliveira <samwolfg13@gmail.com>
2026-04-22 23:11:57 -0300

    Update Cube algorithm

--- assets/base.js
+++ assets/base.js
@@ -10,45 +10,41 @@
 import * as solids from "./solids.js";
 import * as tools from "./tools.js";
 
-// HACK: Testando funções
-console.log(solids.renderCube());
-console.log(solids.renderSphere());
-console.log(solids.renderCilinder());
-
-
 // Version formater
 document.getElementById("version").innerHTML = `
-  Version: ${tools.ARAGEO_VERSION}
+  Versão da Base: ${tools.ARAGEO_VERSION}
 `;
 
 // Get polyhedron info
 const params = new URLSearchParams(window.location.search);
-const solid = params.get("solid");
-const aresta = Number(params.get("aresta"));
+const solidObject = Object.fromEntries(params);
+
+// HACK: Testando funções
+console.log(solidObject);
+console.log(solids.renderCube(solidObject));
+console.log(solids.renderSphere(solidObject));
+console.log(solids.renderCilinder(solidObject));
 
 // Title set
-if (solid) {
-  document.getElementById("solid-name").innerHTML = solid;
+if (solidObject.solid) {
+  document.getElementById("solid-name").innerHTML = solidObject.solid;
 } else {
   document.getElementById("solid-name").innerHTML = "Sem nome!";
 }
 
 // Polyhedron Information
-if (solid && aresta) {
-  // TODO: Create a function for each polyhedron
-  // Pega a tag main e criar uma lsta com informações
-  const container = document.getElementById("solid-info");
-  const ul = document.createElement("ul");
-
-  // Arestas 
-  const liArestas = document.createElement("li");
-  liArestas.innerHTML = `Tamanho das Arestas: ${aresta} <i>cm</i>`;
-  ul.appendChild(liArestas);
-  
-  container.appendChild(ul); // Adiciona li's
+if (solidObject) {
+  // TODO: Criar uma base de dados com JSON
+  if (solidObject.solid == "Cubo") {
+    document.getElementById("solid-info").innerHTML = solids.renderCube(solidObject);
+  } else {
+    document.getElementById("solid-info").innerHTML = `
+      <b>Este sólido não está na base de dados!!!</b>
+    `
+  }
 } else {
   // TODO: Use a createElement() system
   document.getElementById("solid-info").innerHTML = `
-    <b>Estamos sem informações suficientes sobre o sólido</b>
+    <b>Estamos sem informações suficientes sobre o sólido!!!</b>
   `;
 }
--- assets/solids.js
+++ assets/solids.js
@@ -6,14 +6,27 @@
  *
  */
 
-export function renderCube() {
-  return `It works!`;
+export function renderCube(Solid) {
+  const realVolume = Number(Solid.aresta) ** 3;
+  const realArea = (Number(Solid.aresta) ** 2) * 6;
+
+  return `
+  <ul>
+    <li><b>Tamanho do lado do cubo:</b> ${Solid.aresta} <i>cm</i></li>
+    <li><b>Formula do seu Volume:</b> l³ (Volume real)</li>
+    <li><b>Formula do seu Área:</b> 6 * l²</li>
+    <li><b>Volume real do sólido:</b> ${realVolume} <i>cm³</i></li>
+    <li><b>Área real do sólido:</b> ${realArea} <i>cm²</i></li>
+  </ul>
+  `;
 }
 
-export function renderCilinder() {
-  return `It works!`;
+// TODO: Cilinder
+export function renderCilinder(Solid) {
+  return `Sólido: ${Solid.solid}`;
 } 
 
-export function renderSphere() {
-  return `It works!`;
+// TODO: Sphere
+export function renderSphere(Solid) {
+  return `Sólido: ${Solid.solid}`;
 }
