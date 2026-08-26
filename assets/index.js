7730e0659ad6680d21ba7d780f53c1395f79ee73
Samuel-de-Oliveira <samwolfg13@gmail.com>
2026-08-26 19:31:44 -0300

    Include email for contact

--- assets/index.js
+++ assets/index.js
@@ -4,17 +4,17 @@
  *
  */
 
-// TODO: Add translation
+// TODO: Add translation for email contact
 
 import * as tools from "./tools.js";
 import * as info from "./information.js";
+import * as translation from "./translations.js";
 
 document.getElementById("version").innerHTML = `
   Versão da base: ${info.ARAGEO_VERSION}
 `;
 
 document.getElementById("credit").innerHTML = `
-  Criado por: <a href="${info.MAIN_PAGE}">Samuel-de-Oliveira</a><br>
+  Criado por: <a href="${info.MAIN_PAGE}">Samuel-de-Oliveira</a>;<br>
+  Email do desenvolvedor: ${info.DEV_EMAIL}.
 `;
-
-// TODO: Add email for contact
